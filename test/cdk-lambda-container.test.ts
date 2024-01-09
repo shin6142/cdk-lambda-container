de3c0e864aae80ba8b881cc0dc67dd0e4279ea38
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2024-01-09 22:23:07 +0900

    Initial commit

---
+++ test/cdk-lambda-container.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaContainer from '../lib/cdk-lambda-container-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-container-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaContainer.CdkLambdaContainerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
